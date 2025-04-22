86b4c3f06d2dc85706f7ac1cdaf4fc8aa54a1316
felipesora <felipeusora@gmail.com>
2025-04-22 15:50:31 -0300

    Criando o componete Cabecalho

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,14 +1,53 @@
-import styled from 'styled-components/native';
-import { StatusBar } from 'react-native';
+import React from 'react';
+import { View, Text, StyleSheet, TouchableOpacity } from 'react-native';
 
-export const HeaderContainer = styled.View`
-  background-color:rgb(68, 68, 68);
-  padding-top: ${StatusBar.currentHeight}px;
-  padding: 20px;
-`;
+interface HeaderProps {
+  title: string;
+  userName: string;
+  onLogout: () => void;
+}
 
-export const HeaderTitle = styled.Text`
-  color: white;
-  font-size: 20px;
-  font-weight: bold;
-`;
+const Header: React.FC<HeaderProps> = ({ title, userName, onLogout }) => {
+  return (
+    <View style={styles.container}>
+      <Text style={styles.title}>{title}</Text>
+      <View style={styles.rightSection}>
+        <Text style={styles.userName}>{`Bem-vindo, ${userName}`}</Text>
+        <TouchableOpacity onPress={onLogout}>
+          <Text style={styles.logoutText}>Logout</Text>
+        </TouchableOpacity>
+      </View>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    paddingTop: 40, // Ajuste para o cabeçalho não ficar em cima do status bar
+    paddingBottom: 20,
+    paddingHorizontal: 16,
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    backgroundColor: '#546E7A',
+    alignItems: 'center',
+  },
+  title: {
+    fontSize: 24,
+    color: '#fff',
+    fontWeight: 'bold',
+  },
+  rightSection: {
+    flexDirection: 'row',
+    alignItems: 'center',
+  },
+  userName: {
+    color: '#fff',
+    marginRight: 10,
+  },
+  logoutText: {
+    color: '#fff',
+    textDecorationLine: 'underline',
+  },
+});
+
+export default Header;
